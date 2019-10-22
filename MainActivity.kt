7cb44f5ff8021551a89e7184f9a50eaa4f6c631e
arifnugroho1202 <55422535+arifnugroho1202@users.noreply.github.com>
2019-10-22 10:27:21 +0700

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tugase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
